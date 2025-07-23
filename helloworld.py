cd8883efc1be829a4a80ccccf217f9851cb4a744
anonymous62-ru <zidolmax@gmail.com>
2025-07-23 23:20:57 +0200

    Ajout du fichier helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+# Day 1 - 30DaysOfPython Challenge
+print(2 + 3) # addition(+)
+print(3 - 1) # subtraction(-)
+print(2 * 3) # multiplication(*)
+print(3 / 2) # division(/)
+print(3 ** 2) # exponential(**)
+print(3 % 2) # modulus(%)
+print(3 // 2) # Floor division operator(//)
+
+import math
+
+# Coordonnées
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+# Calcul
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print("Distance euclidienne :", distance)
