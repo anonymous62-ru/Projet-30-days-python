ad9436e60f1ae15a56c1163b6456aba47337aa6c
anonymous62-ru <zidolmax@gmail.com>
2025-07-23 23:49:04 +0200

    Ajout des fichiers Day_1 pour le projet 30 jours

---
+++ Day_1/helloworld.py
@@ -0,0 +1,31 @@
+# Jour 1 - Niveau 2
+
+# 1. Vérification de la version Python
+import sys
+print("Version de Python :", sys.version)
+
+# 2. Opérations mathématiques avec 3 et 4
+print("Addition:", 3 + 4)
+print("Soustraction:", 3 - 4)
+print("Multiplication:", 3 * 4)
+print("Modulo:", 3 % 4)
+print("Division:", 3 / 4)
+print("Exponentielle:", 3 ** 4)
+print("Division entière:", 3 // 4)
+
+# 3. Chaînes de texte
+print("Amèvi")
+print("Zidol")
+print("Togo")
+print("Je profite de 30 jours de python")
+
+# 4. Types de données
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finlande']))
+print(type("Amèvi"))
+print(type("Zidol"))
+print(type("Togo"))
+
