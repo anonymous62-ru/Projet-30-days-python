f13260bd463b77f90c14574819f8648c997cc831
anonymous62-ru <zidolmax@gmail.com>
2025-08-08 05:47:03 +0200

    Ajout du dossier Day_17

---
+++ Day_17/Exo_day_17.py
@@ -0,0 +1,8 @@
+noms = ['Finlande', 'Suède', 'Norvège', 'Danemark', 'Islande', 'Estonie', 'Russie']
+
+# Déballage avec l'opérateur *
+nordic_countries, ES, RU = noms[:5], noms[5], noms[6]
+
+print("Pays nordiques :", nordic_countries)
+print("Estonie :", ES)
+print("Russie :", RU)
